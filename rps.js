25f549c1d2661626f0adbce2f752626ad7f03103
Enzorod10 <enzo.rod.er@gmail.com>
2022-01-21 02:46:00 -0500

    rough draft of rps game

--- rps.js
+++ rps.js
@@ -1,7 +1,7 @@
 // create function which will return rock, paper or scissors for computer
 // create function that playus single round of rps
 // this function should take 2 parameters: playerSelection & computerSelection
-// have this function return a string, declaring winner of rthe round
+// have this function return a string, declaring winner of the round
 
 function computerPlay(){
     let pcHand = Math.floor(Math.random() * 3);
@@ -14,24 +14,65 @@ function computerPlay(){
     }
 }
 
-function singleRound(playerSelection, computerSelection){
-    if (playerSelection === computerSelection){
-        return "Tie!";
-    }else if (playerSelection == "rock" & computerSelection == "paper"){
-        return "Computer wins!";
-    }else if (playerSelection == "paper" & computerSelection == "scissors"){
-        return "Computer wins!";
-    }else if (playerSelection == "scissors" & computerSelection == "rock"){
-        return "Computer wins!";
-    }else return "Player wins!";
-}
-for (let i = 0; i < 5; i++){
-    let userOriginalPick = (window.prompt("Please pick a sign: "));
-    let computerSelection = computerPlay();
-    let playerSelection = userOriginalPick.toLowerCase();
+let container = document.querySelector('.container');
+let option1 = document.querySelector('.option1');
+let option2 = document.querySelector('.option2');
+let option3 = document.querySelector('.option3');
+let play = document.querySelector('.play');
 
-    console.log(singleRound(playerSelection, computerSelection));
-    console.log(playerSelection);
-    console.log(computerSelection);
-}
 
+const message = document.querySelector('.message');
+let content = document.createElement('div');
+let content2 = document.createElement('div');
+
+play.textContent = "Play";
+play.addEventListener('click', function(ev){
+    play.textContent = "";
+    play.classList.toggle('play');
+
+    let playerScore = 0;
+    let computerScore = 0;
+
+    container.addEventListener('click', function(ev){
+        //Player picks
+        if (ev.target.classList == "option1"){
+            playerSelection = 'rock';
+        }else if (ev.target.classList == "option2"){
+            playerSelection = 'paper';    
+        }else if (ev.target.classList == "option3"){
+            playerSelection = 'scissors';
+        }else return;
+    
+        //Computer picks
+        computerSelection = computerPlay();
+        //Logic behind winner
+
+        if (playerSelection == computerSelection){
+        }else if (playerSelection == "rock" & computerSelection == "paper"){
+            computerScore++;
+        }else if (playerSelection == "paper" & computerSelection == "scissors"){
+            computerScore++;
+        }else if (playerSelection == "scissors" & computerSelection == "rock"){
+            computerScore++;
+        }
+        else{
+            playerScore++;
+        }
+
+        content2.textContent = "Player Score: " + playerScore;
+        message.appendChild(content2);
+        content.textContent = "Computer Score: " + computerScore;
+        message.appendChild(content);
+    
+        if (playerScore == 5){
+            content.classList.toggle('computerWins');
+            content.textContent = 'Player Wins!';
+            message.appendChild(content);
+        }
+        if (computerScore == 5){
+            content.classList.toggle('computerWins');
+            content.textContent = 'Computer Wins!';
+            message.appendChild(content);
+        }
+    });
+});
